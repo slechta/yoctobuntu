3344d64743ed5d85322fd35a513f16ee5d4a183c
Pavel Slechta <pavel.slechta@zf.com>
2022-01-10 19:43:47 +0100

    Tag v7

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM ubuntu:20.04
-COPY setup.sh .
-RUN ./setup.sh
+COPY ["setup.sh", "/usr/bin/"]
+RUN /usr/bin/setup.sh
 USER builder
--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -eu
 apt-get update
-apt-get -y install gcc g++ make git python3 chrpath cpio diffstat gawk wget locales
+# Yocto required tools
+apt-get -y install gcc g++ make git python3 python3-distutils chrpath cpio diffstat gawk wget locales
 
+# Yocto language sanity check
 sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 dpkg-reconfigure --frontend=noninteractive locales
 update-locale LANG=en_US.UTF-8
 
-
-useradd -s /bin/bash -d /home/builder builder
+# Add user builder
+groupadd --gid 9999 builder
+useradd -s /bin/bash -m -d /home/builder --uid 9999 --gid 9999 -G sudo builder
