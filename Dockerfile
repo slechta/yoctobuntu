295b8d0343416454bca6046560fee5e45c1f0342
Pavel Slechta <pavel.slechta@zf.com>
2022-01-10 18:12:57 +0100

    Add locales v4

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM ubuntu:20.04
-COPY "setup.sh" .
+COPY setup.sh .
 RUN ./setup.sh
 USER builder
--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -eu
 apt-get update
-apt-get -y install gcc g++ make git python3 chrpath cpio diffstat gawk wget
+apt-get -y install gcc g++ make git python3 chrpath cpio diffstat gawk wget locales
+
+sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+dpkg-reconfigure --frontend=noninteractive locales
+update-locale LANG=en_US.UTF-8
+
+
 useradd -s /bin/bash -d /home/builder builder
