2073f4fba912332256b2f4104341ac92ef1953ee
Pavel Slechta <pavel.slechta@zf.com>
2022-01-12 09:51:10 +0100

    Add kmod package

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -eu
 apt-get update
 # Yocto required tools
-apt-get -y install gcc g++ make git python3 python3-distutils chrpath cpio diffstat gawk wget locales
+apt-get -y install gcc g++ make git python3 python3-distutils chrpath cpio diffstat gawk wget locales kmod
 
 # Yocto language sanity check
 sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
@@ -11,4 +11,4 @@ update-locale LANG=en_US.UTF-8
 
 # Add user builder
 groupadd --gid 9999 builder
-useradd -s /bin/bash -m -d /home/builder --uid 9999 --gid 9999 -G sudo builder
+useradd -s /bin/bash -m -d /home/builder --uid 9999 --gid 9999 builder
