980f68d51bd3efaa3b5e11d9c5af125fba5697cb
Pavel Slechta <pavel.slechta@zf.com>
2022-01-25 11:48:17 +0100

    remove sshfs package as ostree does not require it

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -eu
 apt-get update
 # Yocto required tools
 DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get -y install tzdata
-apt-get -y install gcc g++ make git python3 python3-distutils chrpath cpio diffstat gawk wget locales libglib2.0-0 libarchive13 libgpgme11 curl nano sshfs
+apt-get -y install gcc g++ make git python3 python3-distutils chrpath cpio diffstat gawk wget locales libglib2.0-0 libarchive13 libgpgme11 curl nano
 
 # Yocto language sanity check
 sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
